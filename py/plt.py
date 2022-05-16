ebb3c2d80ad69debfe05ca01131c698734bb1187
ephratica <75417931+ephratica@users.noreply.github.com>
2022-05-16 09:59:39 +0800

    前世记忆

--- py/plt.py
+++ py/plt.py
@@ -1,20 +1,19 @@
-# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 
-name_list = ['int', 'float', 'double']
-num_list = [6.23,
-6.23,
-6.24
+name_list = ['2^20', '2^25', '2^28']
+num_list = [1,
+1,
+1
 ]
-num_list1 = [4.07,
-4.08,
-4.07
+num_list1 = [755123/190875,
+32167186/7977684,
+290857940/74198531
 ]
 # l=[num_list1[i]/num_list[i]-1 for i in range(3)]
 # print(l)
-num_list2 = [7.68,
-7.67,
-7.68
+num_list2 = [911283/182123,
+38045323/6429668,
+345035776/61311258
 ]
 # l=[num_list2[i]/num_list[i]-1 for i in range(3)]
 # print(l)
@@ -22,14 +21,17 @@ x = list(range(len(num_list)))
 total_width, n = 0.8, 3
 width = total_width / n
 
-plt.bar(x, num_list, width=width, label='common')
+bar=plt.bar(x, num_list, width=width, label='common')
+plt.bar_label(bar,label_type='edge')
 for i in range(len(x)):
     x[i] = x[i] + width
-plt.bar(x, num_list1, width=width, label='multilink', tick_label=name_list)
+bar=plt.bar(x, num_list1, width=width, label='omp', tick_label=name_list)
+plt.bar_label(bar,label_type='edge')
 for i in range(len(x)):
     x[i] = x[i] + width
-plt.bar(x, num_list2, width=width, label='recursion', tick_label=name_list)
-plt.ylabel("cache-loads(%)")
-plt.xlabel("data type")
+bar=plt.bar(x, num_list2, width=width, label='omp simd', tick_label=name_list)
+plt.bar_label(bar,label_type='edge')
+plt.ylabel("times/second")
+plt.xlabel("n")
 plt.legend()
 plt.show()
